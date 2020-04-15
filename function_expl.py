c408c8b52546df14e50560f352e8de76e23fbf0c
Lâm Mạnh <manhlamvepj7543@gmail.com>
2020-04-15 21:48:32 +0700

    funtion_explain

---
+++ function_expl.py
@@ -0,0 +1,22 @@
+import math
+
+print(math.pi)
+print(math.e)
+
+#lam tron 2 chu so sau dau phay
+print(round(math.e, 2))
+
+#in ra so lam tron xuong cua 1.9
+print(math.floor(1.9))
+
+#in ra so lam tron len cua 2.1
+print(math.ceil(2.1))
+
+#lay gia tri nguyen cat bo phan thap phan sau dau phay cua so thuc
+print(math.trunc(-1.9))
+
+#in ra giai thua cua 5
+print(math.factorial(5))
+
+#in ra uoc chung lon nhat cua hai so nguyen
+print(math.gcd(12, 18))
